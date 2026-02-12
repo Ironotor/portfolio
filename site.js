c2d57fad6dd940aac368870f71d106f6eddadda4
Ironotor <andarowa@outlook.com>
2026-02-12 10:24:07 -0500

    Initial commit. Test of js based html includes

---
+++ site.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", includeHTML);
+/**
+ * Finds all elements with an include-html attribute and attampts an ajax call to append html contents
+ */
+function includeHTML() {
+    var elements = document.querySelectorAll('[include-html]');
+    elements.forEach((element) => {
+        file = element.getAttribute("include-html");
+        fetch(file).then(file => {
+            file.text().then(content => {
+                element.insertAdjacentHTML('afterend', content);
+                element.removeAttribute("include-html");
+            });
+        });
+        return;
+    });
+}
